49637a37d9ed53cc5bb0b81d447c66db83bedd65
sikun21[Ash <johndoe@example.com>
2022-01-29 15:12:55 +0530

    myred com

--- myjavaregistationpage.java
+++ myjavaregistationpage.java
@@ -1,2 +1,10 @@
+<<<<<<< Updated upstream
 this is my registation pagmy 
 my manager work
+=======
+this is my registation page
+1. day lines of java code
+2. java line of java code
+3. day lines of java code
+4. day line of java code
+5.day of lines of code
